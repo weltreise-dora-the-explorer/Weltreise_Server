ac42dedc5665fdf13c00490474a187e94c2ce88d
ph.mazarisgmail.com <ph.mazaris@gmail.com>
2026-05-30 13:32:33 +0200

    feat(report): mustSkipNextTurn + shakeCheatReported in PlayerState

--- src/main/java/at/aau/serg/websocketdemoserver/game/models/PlayerState.java
+++ src/main/java/at/aau/serg/websocketdemoserver/game/models/PlayerState.java
@@ -27,6 +27,8 @@ public class PlayerState {
     private List<City> visitedCities = new ArrayList<>();
     private int freePassCount;
     private boolean shakeCheatUsedThisRoll = false;
+    private boolean shakeCheatReported = false;
+    private boolean mustSkipNextTurn = false;
 
     /**
      * Konstruktor nur mit playerId (für Lobby-Join).
